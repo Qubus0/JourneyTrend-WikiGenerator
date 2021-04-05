c01f0b52ea2503448062bb1c3a36401eb03be404
Qubus0 <steen.rickmer@gmx.de>
2021-04-05 18:36:14 +0200

    updated action

--- generate_terraria_mod_wiki/generate_terraria_mod_wiki.py
+++ generate_terraria_mod_wiki/generate_terraria_mod_wiki.py
@@ -30,7 +30,7 @@ class Wiki:
         self.source_dir: str = source_dir
         self.vanity_source_dir: str = os.path.join(source_dir, "Items", "Vanity")
         self.repo: Repo = None
-        self.package_name = "generate_terraria_mod_wiki"
+        # self.package_name = "generate_terraria_mod_wiki"
         self.wiki_url_base: str = "/Qubus0/JourneyTrend/wiki/"
         self.repo_url_base: str = "/Qubus0/JourneyTrend/tree/master/"
         self.repo_url_file_base: str = "/Qubus0/JourneyTrend/blob/master/"
